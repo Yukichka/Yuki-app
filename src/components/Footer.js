a75b0ca54ae9b752a67f9d425672f5115ef5ec42
Yukichka <yukiinsw@gmail.com>
2020-04-13 19:02:45 +0100

    footer year updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export class Footer extends React.Component {
   render() {
     return (
-      <div className="footer">© 2019 <Link to ="/" className="footer-link"><span>Yuki Inasawa</span>.</Link> All Right Reserved.</div>
+      <div className="footer">
+        © {new Date().getFullYear()}{" "}
+        <Link to="/" className="footer-link">
+          <span>Yuki Inasawa</span>.
+        </Link>{" "}
+        All Right Reserved.
+      </div>
     );
   }
 }
